65260d30e7955acdf6a7609367302f7b09812a2e
Pablo villalpando alvarez <pablitova@gmail.com>
2021-08-15 18:02:12 -0500

    step 1 map config

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,15 @@
+// We create the tile layer that will be the background of our map.
+console.log("Step 1");
+
+var graymap = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 500,
+    maxZoom: 20,
+    zoomOffset: -2,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  }
+);
